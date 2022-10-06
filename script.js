846476e41853c0ad31aaea753ac229ac6f54624d
Rudra Vengurlekar <107509068+QuantRV@users.noreply.github.com>
2022-10-06 18:19:42 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// document.querySelector('iframe').contentWindow.document.querySelector('.ytp-chrome-top, .ytp-show-cards-title').style.visibility = "hidden";
+
+function preloaderFunction() {
+    document.querySelector(".Rpreload").style.display = "none";
+}
+
+function myFunction() {
+    preloaderFunction();
+}
